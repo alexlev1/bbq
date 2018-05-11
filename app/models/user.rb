54ec290d172cac59ae311ed10a639a83e3e824c0
Alex Levashov <alevash1@gmail.com>
2018-05-11 16:39:27 +0300

    Add comments and subscriptions form partials, add ru locale

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,10 +10,16 @@ class User < ApplicationRecord
 
   before_validation :set_name, on: :create
 
+  # При регистрации пользователя подхватываем его данные, если уже была анонимная активность на сайте
+  after_commit :link_subscriptions, on: :create
+
   private
 
   def set_name
     self.name = "Товарисч №#{rand(777)}" if self.name.blank?
   end
 
+  def link_subscriptions
+    Subscription.where(user_id: nil, user_email: self.email).update_all(user_id: self.id)
+  end
 end
