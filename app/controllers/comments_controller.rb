2db457351158743b525b75583b83dcf73c714a37
Alex Levashov <alevash1@gmail.com>
2018-05-29 21:21:28 +0300

    Fix bug with comment notify for unauthorized users

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -44,7 +44,7 @@ class CommentsController < ApplicationController
     # собираем всех подписчиков и автора события в массив мэйлов, исключаем повторяющиеся
     all_emails = (event.subscriptions.map(&:user_email) + [event.user.email]).uniq
 
-    all_emails.delete(comment.user.email)
+    all_emails.delete(comment.user.try(:email))
 
     # XXX: Этот метод может выполняться долго из-за большого числа подписчиков
     # поэтому в реальных приложениях такая задача будет выносится в background задачи!
